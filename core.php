16085bab6b5ca61e926cc1292917a5a74e1db1ef
josbenper <josbenper@gmail.com>
2013-04-18 12:28:25 +0300

    Create core.php

---
+++ core.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
+<?php include ('js/js.php'); ?>
+<?php include ('echi.php'); ?>
+<?php include ('prindemo.php'); ?>
+<?php include ('senorcaos.php'); ?>
